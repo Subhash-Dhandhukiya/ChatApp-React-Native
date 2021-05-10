b77b8d9fc1b7ccfd9f7e311d91a8e63b012a0c7a
Subhash Dhandhukiya <subhashdhandhukiya69@gmail.com>
2021-05-10 19:48:25 +0530

    New Screen Added

--- src/Container/Setting/index.js
+++ src/Container/Setting/index.js
@@ -1,11 +1,49 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react'
-import { View, Text ,StyleSheet} from 'react-native'
+import { View, Text ,StyleSheet, Alert} from 'react-native'
+import { clearAsyncStorage } from '../../AsyncStorage';
+import Option from '../../Component/Option';
+import { LogoutUser } from '../../Network';
 import { color } from '../../Utility';
+import { LOGIN } from '../../Utility/Constant/Route';
 
-const Setting = () => {
+const Setting = ({navigation}) => {
+    
+    const handelLogout=()=>{
+        Alert.alert(
+            "Logout",
+            "Are you sure to logout",
+            [
+                {
+                    text:"Yes",
+                    onPress:()=>logout()
+                },
+                {
+                    text:"No",
+                    
+                }
+            ]
+        )
+    }
+    
+    //Logout
+    const logout=()=>{
+        LogoutUser()
+        .then(()=>{
+            clearAsyncStorage()
+            .then(()=>{
+                navigation.replace(LOGIN)
+            })
+            .catch(error=>console.log(error))
+        })
+        .catch((error)=>alert(error))
+    }
+
     return (
         <View style={styles.container}>
-            <Text>This is Setting Screen</Text>
+            <Option title="Profile" onPress={()=>handelLogout()}/>
+            <Option title="Theme" onPress={()=>handelLogout()}/>
+            <Option title="Logout" onPress={()=>handelLogout()}/>
         </View>
     )
 }
@@ -15,8 +53,6 @@ export default Setting;
 const styles=StyleSheet.create({
     container:{
         flex:1,
-        justifyContent:'center',
-        alignItems:'center',
         backgroundColor:color.WHITE
     }
 })
