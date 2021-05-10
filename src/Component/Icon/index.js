b48149a13d09dbbf5b35e83427717aee0f81a44a
Subhash Dhandhukiya <subhashdhandhukiya69@gmail.com>
2021-05-10 19:47:03 +0530

    New Icon added

--- src/Component/Icon/index.js
+++ src/Component/Icon/index.js
@@ -2,10 +2,12 @@ import BackIcon from './Back'
 import ChatBoxIcon from './ChatBox'
 import SettingIcon from './Setting'
 import VerticalDotsIcon from './VerticalDot'
+import GreaterThenIcon from './GreaterThen'
 
 export {
     BackIcon,
     ChatBoxIcon,
     SettingIcon,
-    VerticalDotsIcon
+    VerticalDotsIcon,
+    GreaterThenIcon
 }
