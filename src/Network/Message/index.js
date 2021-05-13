1cd2daa57bc7a93c4ff3edb95393056c442af835
Subhash Dhandhukiya <subhashdhandhukiya69@gmail.com>
2021-05-13 18:12:09 +0530

    add Message code

--- src/Network/Message/index.js
+++ src/Network/Message/index.js
@@ -2,39 +2,43 @@ import firebase from '../../Firebase/config'
 
 export const senderMsg = async (msgValue, currentUserId, guestUserId, img) => {
     try {
-        return await firebase
-            .database()
-            .ref('message/' + currentUserId)
-            .child(guestUserId)
-            .push({
-                message: {
-                    sender: currentUserId,
-                    reciver: guestUserId,
-                    msg: msgValue,
-                    img: img
-                }
-            })
+      return await firebase
+        .database()
+        .ref('messeges/' + currentUserId)
+        .child(guestUserId)
+        .push({
+          messege: {
+            sender: currentUserId,
+            reciever: guestUserId,
+            msg: msgValue,
+            img: img,
+          },
+        });
     } catch (error) {
-        return error;
+      return error;
     }
-}
-
-
-export const recieveMsg = async (msgValue, currentUserId, guestUserId, img) => {
+  };
+  
+  export const recieverMsg = async (
+    msgValue,
+    currentUserId,
+    guestUserId,
+    img,
+  ) => {
     try {
-        return await firebase
-            .database()
-            .ref('message/' + guestUserId)
-            .child(currentUserId)
-            .push({
-                message: {
-                    sender: currentUserId,
-                    reciver: guestUserId,
-                    msg: msgValue,
-                    img: img
-                }
-            })
+      return await firebase
+        .database()
+        .ref('messeges/' + guestUserId)
+        .child(currentUserId)
+        .push({
+          messege: {
+            sender: currentUserId,
+            reciever: guestUserId,
+            msg: msgValue,
+            img: img,
+          },
+        });
     } catch (error) {
-        return error;
+      return error;
     }
-}
+  };
--- src/Network/index.js
+++ src/Network/index.js
@@ -1,6 +1,6 @@
 import loginRequest from "./Login";
 import LogoutUser from "./Logout";
-import { recieveMsg, senderMsg } from "./Message";
+import { recieverMsg, senderMsg } from "./Message";
 import signupRequest from "./Signup";
 import {AddUser,UpdateUser} from "./User"
 
@@ -12,5 +12,5 @@ export {
     LogoutUser,
     UpdateUser,
     senderMsg,
-    recieveMsg
+    recieverMsg
 }
