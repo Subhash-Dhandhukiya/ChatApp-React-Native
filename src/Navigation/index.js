271b4c4dc480586b850b87cacb2604b092214df9
Subhash Dhandhukiya <subhashdhandhukiya69@gmail.com>
2021-05-15 10:09:39 +0530

    New code added into navigation

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -3,10 +3,11 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
+
 //import all Screen
-import { Login, Signup, Dashboard, Welcome, Splash, Setting, Profile, ShowFullImg ,Chat} from '../Container';
+import { Login, Signup, Dashboard, Welcome, Splash, Setting, Profile, ShowFullImg, Chat, Theme } from '../Container';
 import { color } from '../Utility';
-import { LOGIN, SIGNUP, WELCOME, DASHBOARD, SPLASH, SETTING, BOTTOMTAB, PROFILE, DASHBOARDSCREEN, SETTINGSCREEN, SHOWFULLIMG, CHAT } from '../Utility/Constant/Route'
+import { LOGIN, SIGNUP, WELCOME, DASHBOARD, SPLASH, SETTING, PROFILE, DASHBOARDSCREEN, SHOWFULLIMG, CHAT, THEME } from '../Utility/Constant/Route'
 import { ChatBoxIcon, SettingIcon } from '../Component/Icon'
 
 const Stack = createStackNavigator();
@@ -16,9 +17,9 @@ const tabBarOptions = {
     showLabel: true,
     activeTintColor: color.BLUE,
     inactiveTintColor: color.ICON,
-    keyboardHidesTabBar:true,
-    style:{
-      position:'relative'
+    keyboardHidesTabBar: true,
+    style: {
+        position: 'relative'
     }
 }
 
@@ -26,141 +27,110 @@ const tabBarListeners = ({ navigation, route }) => ({
     tabPress: () => navigation.navigate(route.name),
 });
 
-//Dashboard Screen Navigation 
-const DashboardStack = createStackNavigator();
-const DashboardNavigator = ({ }) => {
-    return (
-        <DashboardStack.Navigator initialRouteName={Dashboard}>
-            <DashboardStack.Screen
-                name={DASHBOARD}
-                component={Dashboard}
-                options={{
-                    headerLeft: null
-                }}
-            />
 
-            <DashboardStack.Screen
-                name={SHOWFULLIMG}
-                component={ShowFullImg}
-                options={{
-                    headerShown: null
-                }}
-            />
-
-            <DashboardStack.Screen
-                name={CHAT}
-                component={Chat}
-            />
-        </DashboardStack.Navigator>
-    );
-}
-
-
-
-//Setting Screen Navigation
-const SettingStack = createStackNavigator();
-const SettingNavigator = ({ }) => {
-    return (
-        <SettingStack.Navigator initialRouteName={Setting}>
-            <SettingStack.Screen
-                name={SETTING}
-                component={Setting}
-                options={{
-                    headerLeft: null,
-                }}
-            />
-            <SettingStack.Screen
-                name={PROFILE}
-                component={Profile}
-                options={{ headerShown: null }}
-            />
-        </SettingStack.Navigator>
-    );
-}
-
-
-function MainScreen() {
+function HomeTabs() {
     return (
         <Tab.Navigator
-            initialRouteName={DashboardNavigator}
+            initialRouteName={Dashboard}
             tabBarOptions={tabBarOptions}
         >
-            <Tab.Screen
-                name={DASHBOARDSCREEN}
-                component={DashboardNavigator}
+            <Tab.Screen 
+                name={DASHBOARD} 
+                component={Dashboard} 
                 listeners={tabBarListeners}
                 options={{
-                    tabBarIcon: ({ color }) => (
+                    tabBarIcon:({color})=>(
                         <ChatBoxIcon
                             fill={color}
                             height={25}
                             width={25}
                         />
                     ),
+                    title:'Chat'
                 }}
             />
-            <Tab.Screen
-                name={SETTINGSCREEN}
-                component={SettingNavigator}
+            <Tab.Screen 
+                name={SETTING}
+                component={Setting} 
                 listeners={tabBarListeners}
                 options={{
-                    tabBarIcon: ({ color }) => (
+                    tabBarIcon:({color})=>(
                         <SettingIcon
                             fill={color}
                             height={25}
                             width={25}
                         />
-                    )
+                    ),
+                    title:'Setting'
                 }}
             />
         </Tab.Navigator>
-    );
+    )
 }
 
 
-function NavContainer() {
+const NavContainer = () => {
     return (
-        <NavigationContainer independent={true}>
-            <Stack.Navigator
-                initialRouteName={SPLASH}
-                screenOptions={{
-                    headerShown: false,
-                    headerStyle: { backgroundColor: color.WHITE },
-                    headerTintColor: color.BLUE,
-                    headerTitleAlign: 'center',
-                    headerTitleStyle: {
-                        fontSize: 30
-                    }
-
-                }}
-            >
-                <Stack.Screen
-                    name={SPLASH}
-                    component={Splash}
-                    options={{ headerShown: false }}
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen 
+                    name={SPLASH} 
+                    component={Splash} 
+                    options={{headerShown:false}}
                 />
-                <Stack.Screen
-                    name={WELCOME}
-                    component={Welcome}
-                    options={{ headerShown: false }}
+                <Stack.Screen 
+                    name={WELCOME} 
+                    component={Welcome} 
+                    options={{headerShown:false}}
                 />
-                <Stack.Screen
-                    name={LOGIN}
-                    component={Login}
-                    options={{ headerShown: false }}
+                <Stack.Screen 
+                    name={LOGIN} 
+                    component={Login} 
+                    options={{headerShown:false}}
                 />
-                <Stack.Screen
-                    name={SIGNUP}
-                    component={Signup}
-                    options={{ headerShown: false }}
+                <Stack.Screen 
+                    name={SIGNUP} 
+                    component={Signup} 
+                    options={{headerShown:false}}
                 />
-                <Stack.Screen
-                    name={BOTTOMTAB}
-                    component={MainScreen}
+                <Stack.Screen 
+                    name={DASHBOARDSCREEN} 
+                    component={HomeTabs}
+                    options={{headerShown:false}}
                 />
+                <Stack.Screen 
+                    name={PROFILE} 
+                    component={Profile}
+                    options={{headerShown:false}}
+                />
+                <Stack.Screen 
+                    name={SHOWFULLIMG} 
+                    component={ShowFullImg} 
+                    options={{headerShown:false}}
+                />
+                <Stack.Screen 
+                    name={CHAT} 
+                    component={Chat}
+                />
+                <Stack.Screen 
+                    name={THEME} 
+                    component={Theme}
+                    options={{headerShown:false}}
+                />
+                
             </Stack.Navigator>
         </NavigationContainer>
     );
+}
+
+const getHeaderTitle = (route) => {
+    if (route === DASHBOARD) {
+        return "Chat";
+    } else if (route === SETTING) {
+        return "Setting"
+    } else {
+        return route;
+    }
 }
 
 export default NavContainer;
