72ebf5e94646a8b7551f5ca82cb2e1149b770ac6
Subhash Dhandhukiya <subhashdhandhukiya69@gmail.com>
2021-05-15 18:51:50 +0530

    Added new code in App.js

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import NavContainer from './src/Navigation'
 import Loader from './src/Component/Loader'
-import {StatusBar} from 'react-native'
+import { StatusBar } from 'react-native'
 import { StoreProvider } from './src/Context/store'
-
+import { ThemeContext } from './src/ThemeContext'
 const App = () => {
+
+  const [theme,setTheme]=useState("Light")
+
   return (
-    <StoreProvider>
-      <StatusBar barStyle="light-content"/>
-      <NavContainer/>
-      <Loader/>
-    </StoreProvider>
+    <ThemeContext.Provider value={{theme,setTheme}}>
+      <StoreProvider>
+        <StatusBar barStyle="light-content" />
+        <NavContainer />
+        <Loader />
+      </StoreProvider>
+    </ThemeContext.Provider>
+
   )
 }
 
