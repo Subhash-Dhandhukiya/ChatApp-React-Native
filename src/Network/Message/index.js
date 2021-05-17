c08976e2032edd070aea7929933649d576556c77
Subhash Dhandhukiya <subhashdhandhukiya69@gmail.com>
2021-05-17 12:07:00 +0530

    new code added

--- src/Network/Message/index.js
+++ src/Network/Message/index.js
@@ -1,44 +1,48 @@
 import firebase from '../../Firebase/config'
 
-export const senderMsg = async (msgValue, currentUserId, guestUserId, img) => {
-    try {
-      return await firebase
-        .database()
-        .ref('messeges/' + currentUserId)
-        .child(guestUserId)
-        .push({
-          messege: {
-            sender: currentUserId,
-            reciever: guestUserId,
-            msg: msgValue,
-            img: img,
-          },
-        });
-    } catch (error) {
-      return error;
-    }
-  };
+export const senderMsg = async (msgValue, currentUserId, guestUserId, img,seen_msg) => {
+  try {
+    return await firebase
+      .database()
+      .ref('messeges/' + currentUserId)
+      .child(guestUserId)
+      .push({
+        messege: {
+          sender: currentUserId,
+          reciever: guestUserId,
+          msg: msgValue,
+          img: img,
+          seen_msg:seen_msg
+        },
+      });
+  } catch (error) {
+    return error;
+  }
+};
   
-  export const recieverMsg = async (
-    msgValue,
-    currentUserId,
-    guestUserId,
-    img,
-  ) => {
-    try {
-      return await firebase
-        .database()
-        .ref('messeges/' + guestUserId)
-        .child(currentUserId)
-        .push({
-          messege: {
-            sender: currentUserId,
-            reciever: guestUserId,
-            msg: msgValue,
-            img: img,
-          },
-        });
-    } catch (error) {
-      return error;
-    }
-  };
+  
+export const recieverMsg = async (
+  msgValue,
+  currentUserId,
+  guestUserId,
+  img,
+  seen_msg
+) => {
+  try {
+    return await firebase
+      .database()
+      .ref('messeges/' + guestUserId)
+      .child(currentUserId)
+      .push({
+        messege: {
+          sender: currentUserId,
+          reciever: guestUserId,
+          msg: msgValue,
+          img: img,
+          seen_msg:seen_msg
+        },
+      });
+  } catch (error) {
+    return error;
+  }
+};
